24137a7485eb119d451d123337f47d73dbe2560c
UplandPestControl <mhodges@uccs.edu>
2015-11-17 22:26:39 -0700

    Included Images/Logos/Icons, fixed navbar, footer, added content for benefits & about page

--- Upland Pest Control/Controllers/HomeController.cs
+++ Upland Pest Control/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,18 +15,25 @@ namespace Upland_Pest_Control.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Upland Pest Control";
-
-            return View();
+        public JsonResult EmailCustomerInfo(string customerName, string customerEmail, string customerPhone, string additionalNotes) {
+            string result = "";
+            try
+            {
+                MailMessage mailMessage = new MailMessage();
+                mailMessage.To.Add("mike_hodges44@yahoo.com");
+                mailMessage.From = new MailAddress(customerEmail);
+                mailMessage.Subject = "Upland Pest Control Customer Inquiry";
+                mailMessage.Body = additionalNotes;
+                SmtpClient smtpClient = new SmtpClient("127.0.0.1");
+                //smtpClient.Credentials = new NetworkCredential("Administrator", "UplandP3stC0ntrol");
+                smtpClient.Send(mailMessage);
+                result = "Success";
+            }
+            catch (Exception ex)
+            {
+                result = "Could not send the e-mail - error: " + ex.Message;
+            }
+            return Json(result, JsonRequestBehavior.AllowGet);
         }
     }
 }
--- Upland Pest Control/Scripts/site.js
+++ Upland Pest Control/Scripts/site.js
@@ -25,15 +25,32 @@ in .Net, but in case we want to make a full-blown SPA, this code is here */
 //    $locationProvider.html5Mode(true);
 //});
 
-app.controller("appCtrl", function ($scope) {
+app.controller("appCtrl", function ($scope, $http) {
     var imgNum = Math.floor(((Math.random() * 100) % 4) + 1);
     $scope.backgroundImage = "bg-img-" + imgNum;
-
+    $scope.customerName = "";
+    $scope.customerEmail = "";
+    $scope.customerPhone = "";
+    $scope.additionalNotes = "";
     /* Code to programmatically change the background. Not useful at the moment,
     but may be useful down the road. */
     //$scope.changeBackgroundImage = function (imgNumber) {
     //    $scope.backgroundImage = "bg-img-" + imgNumber;
     //}
+    $scope.sendEmail = function () {
+        $http.post("/Home/EmailCustomerInfo",
+            {
+                customerName: $scope.customerName,
+                customerEmail: $scope.customerEmail,
+                customerPhone: $scope.customerPhone,
+                additionalNotes: $scope.additionalNotes
+            }).then(function successCallback(response) {
+                console.log(response.data);
+            }, function failureCallback(response) {
+                console.log(response.data);
+            }
+        );
+    }
 });
 
 app.controller("homeCtrl", function ($scope) {
