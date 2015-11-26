f4f8fdd111665b0612f7839c22e03270b9221330
UplandPestControl <mhodges@uccs.edu>
2015-11-26 00:03:00 -0700

    Finished About, Contact, FAQ, layouts. Added Services, rearranged site content

--- Upland Pest Control/Scripts/site.js
+++ Upland Pest Control/Scripts/site.js
@@ -27,3 +27,98 @@ app.controller("appCtrl", function ($scope, $http) {
         );
     }
 });
+
+$(function () {
+    var offset = $(".navbar").offset().top;
+    function stickyNav() {
+        if ($(window).width() > 1205 && $(window).scrollTop() > offset) {
+            $(".navbar").addClass("sticky");
+        }
+        else if ($(window).width() <= 1205) {
+            $(".navbar").addClass("sticky");
+        }
+        else {
+            $(".navbar").removeClass("sticky");
+        }
+    }
+    stickyNav();
+    $(window).on("scroll resize", function () {
+        stickyNav();
+    });
+    var inputs = function (element) {
+        if (element.val() != "") {
+            element.parent().addClass("show-label");
+        } else {
+            element.parent().removeClass("show-label");
+        }
+    }
+    $.each($(".inline-label-input"), function () {
+        inputs($(this));
+    });
+    $(".inline-label-input").on("input propertychange change", function () {
+        inputs($(this));
+    });
+
+    /** 
+        Accordion Widget - http://codepen.io/chriswrightdesign/pen/cmanI
+    **/
+    var d = document,
+accordionToggles = d.querySelectorAll('.js-accordionTrigger'),
+setAria,
+setAccordionAria,
+switchAccordion,
+touchSupported = ('ontouchstart' in window),
+pointerSupported = ('pointerdown' in window);
+
+    skipClickDelay = function (e) {
+        e.preventDefault();
+        e.target.click();
+    }
+
+    setAriaAttr = function (el, ariaType, newProperty) {
+        el.setAttribute(ariaType, newProperty);
+    };
+    setAccordionAria = function (el1, el2, expanded) {
+        switch (expanded) {
+            case "true":
+                setAriaAttr(el1, 'aria-expanded', 'true');
+                setAriaAttr(el2, 'aria-hidden', 'false');
+                break;
+            case "false":
+                setAriaAttr(el1, 'aria-expanded', 'false');
+                setAriaAttr(el2, 'aria-hidden', 'true');
+                break;
+            default:
+                break;
+        }
+    };
+    //function
+    switchAccordion = function (e) {
+        e.preventDefault();
+        var thisAnswer = e.target.parentNode.nextElementSibling;
+        var thisQuestion = e.target;
+        if (thisAnswer.classList.contains('is-collapsed')) {
+            setAccordionAria(thisQuestion, thisAnswer, 'true');
+        } else {
+            setAccordionAria(thisQuestion, thisAnswer, 'false');
+        }
+        thisQuestion.classList.toggle('is-collapsed');
+        thisQuestion.classList.toggle('is-expanded');
+        thisAnswer.classList.toggle('is-collapsed');
+        thisAnswer.classList.toggle('is-expanded');
+
+        thisAnswer.classList.toggle('animateIn');
+    };
+    for (var i = 0, len = accordionToggles.length; i < len; i++) {
+        if (touchSupported) {
+            accordionToggles[i].addEventListener('touchstart', skipClickDelay, false);
+        }
+        if (pointerSupported) {
+            accordionToggles[i].addEventListener('pointerdown', skipClickDelay, false);
+        }
+        accordionToggles[i].addEventListener('click', switchAccordion, false);
+    }
+    /**
+        End Accordion Widget
+    **/
+});
